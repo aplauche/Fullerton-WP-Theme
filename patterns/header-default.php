f8a7920af7165888029dda5d33628e8380b7409d
Anton Plauche <antonplauche92@gmail.com>
2022-12-10 13:33:38 -0600

    add link to header logo

--- patterns/header-default.php
+++ patterns/header-default.php
@@ -7,10 +7,10 @@
  * Viewport Width: 1280
  */ ?>
 
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0px","bottom":"0px"}},"border":{"radius":"0px"}},"className":"is-style-default","layout":{"inherit":true}} -->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0px","bottom":"0px"}},"border":{"radius":"0px"}},"className":"is-style-default","layout":{"inherit":true,"type":"constrained"}} -->
 <div class="wp-block-group alignfull is-style-default" style="border-radius:0px;padding-top:0px;padding-bottom:0px"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"32px","bottom":"32px"}},"border":{"radius":"16px"}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignwide" style="border-radius:16px;padding-top:32px;padding-bottom:32px"><!-- wp:image {"width":75,"height":75,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full is-resized"><img src="<?php echo get_stylesheet_directory_uri( ) . '/assets/images/sample-logo.png'?>" alt="" width="75" height="75"/></figure>
+<div class="wp-block-group alignwide" style="border-radius:16px;padding-top:32px;padding-bottom:32px"><!-- wp:image {"width":75,"height":75,"sizeSlug":"full","linkDestination":"custom"} -->
+<figure class="wp-block-image size-full is-resized"><a href="<?php echo home_url( );?>"><img src="<?php echo get_stylesheet_directory_uri( ) . '/assets/images/sample-logo.png'?>" alt="" width="75" height="75"/></a></figure>
 <!-- /wp:image -->
 
 <!-- wp:navigation {"textColor":"base","hasIcon":false,"layout":{"type":"flex","orientation":"horizontal"},"style":{"spacing":{"blockGap":"20px"}}} /--></div>
